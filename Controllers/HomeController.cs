310943561ef65f951ff2534abe45339ffba807e0
Evan <calliaevan12@gmail.com>
2016-12-09 17:23:23 -0800

    deployment

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -11,7 +11,7 @@ namespace evanCalliaPortfolio.Controllers
         [Route("")]
         public IActionResult Index()
         { 
-            return View();
+            return View("index");
         }
     }
 }
