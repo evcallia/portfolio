ff758b53f2d535be9a474331c6b4a3c3c1fb69f0
Evan <calliaevan12@gmail.com>
2016-12-09 17:14:06 -0800

    reinitializing repository. Added everything except projects.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace evanCalliaPortfolio.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        [Route("")]
+        public IActionResult Index()
+        { 
+            return View();
+        }
+    }
+}
